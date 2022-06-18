2e1807e2e7213291ebb41c6fcf928104f44c1431
Jonathan Espinosa <jon_esp_esq@hotmail.com>
2022-06-17 21:48:56 -0500

    feat: lambda register users RM-8

--- register/go.mod
+++ register/go.mod
@@ -2,4 +2,7 @@ module register
 
 go 1.18
 
-require github.com/aws/aws-lambda-go v1.32.0
+require (
+	github.com/aws/aws-lambda-go v1.32.0
+	github.com/lib/pq v1.10.6
+)
--- register/main.go
+++ register/main.go
@@ -1,28 +1,83 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	_ "github.com/lib/pq"
 )
 
-type Message struct {
-	Nickname string `json:"nickname"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
-	Greeting string `json:"greeting"`
+type Usuarios struct {
+	First_name string `json:"first_name"`
+	Last_name  string `json:"last_name"`
+	Birthday   string `json:"birthday"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	Email      string `json:"email"`
+	City       string `json:"city"`
+	Code_zip   string `json:"code_zip"`
+	State      string `json:"state"`
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var nick Message
-	_ = json.Unmarshal([]byte(request.Body), &nick)
+type Response struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
 
-	newMes := Message{
-		Nickname: nick.Nickname,
-		Name:     "Jonathan Espinosa",
-		Age:      25,
-		Greeting: "Handshake Update",
+const (
+	// host     = "database-rescueme.caizuq7lxwzo.us-east-1.rds.amazonaws.com"
+	// port     = 5431
+	// password = "pws$69%2022LaMaou"
+	// dbname = "rescuemedb"
+	// user     = "masteruser"
+	host     = "localhost"
+	dbname   = "postgres"
+	port     = 54321
+	user     = "postgres"
+	password = ""
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	var u Usuarios
+	_ = json.Unmarshal([]byte(request.Body), &u)
+
+	// val := Usuarios{
+	// 	First_name: "JOathan",
+	// 	Last_name:  "espinsoa",
+	// 	Birthday:   "17 marzo",
+	// 	Username:   "asfafASDF",
+	// 	Password:   "1235413FG",
+	// 	Email:      "jonathan@hotmail.com",
+	// 	City:       "zitacuaro",
+	// 	Code_zip:   "61518",
+	// 	State:      "Michoacn",
+	// }
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
+	defer db.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	db.Exec(
+		"INSERT INTO users (first_name, last_name, birthday, username, password, email, city, code_zip, state) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);",
+		u.First_name, u.Last_name, u.Birthday, u.Username, u.Password, u.Email, u.City, u.Code_zip, u.State)
+	fmt.Println("User created successfully")
+
+	// rows, _ := db.Query("SELECT * FROM users;")
+	// value := Usuarios{}
+	// for rows.Next() {
+	// 	err = rows.Scan(&value.First_name, &val.Last_name, &value.State)
+	// 	fmt.Println(value.First_name, value.Last_name, value.State)
+	// }
+	newMes := Response{
+		Message:  "User successfully created",
+		Username: u.Username,
 	}
 	a, _ := json.Marshal(newMes)
 
@@ -33,6 +88,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		Body:       string(a),
 		StatusCode: 200,
 	}, nil
+
 }
 
 func main() {
